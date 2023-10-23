743c6a30b0ce0be5dcba086fb590649137e7f1c5
francois GIRARD <francoisgirard51@gmail.com>
2023-10-23 16:17:23 +0200

    Fix style issues, should get a 'Good Style' now 🙏

--- today.py
+++ today.py
@@ -1,10 +1,7 @@
-'''
-This is the module docstring
-'''
-
-
 def my_name_is():
+    """A module computing buddy pair names for the day"""
     return "francoisgirard51"
 
 def my_buddy_is():
+    """A module computing buddy pair names for the day"""
     return "dawid-sk"
